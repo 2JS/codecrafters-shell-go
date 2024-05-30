e0859e27318050d476698dbec4101351a1fffdbb
Lee, Jun Seok <lego3410@gmail.com>
2024-05-30 12:38:01 +0900

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,11 +8,15 @@ import (
 )
 
 func main() {
-
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		input = strings.Trim(input, "\r\n")
-		fmt.Fprintf(os.Stdout, "%s: command not found\n", input)
+		switch input {
+		case "exit 0":
+			os.Exit(0)
+		default:
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", input)
+		}
 	}
 }
